d2c712f2f0ba68bf8b0a0c5e04424a1f2cf44864
Nivas Muthu M G <nivasmuthu452@gmail.com>
2021-08-13 08:13:54 +0530

    Moved from ListView to PageView for Ongoing Events

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:nss_tracker/views/expanded_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _currentOngoingPageIndex = 0;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(
+        initialPage: _currentOngoingPageIndex,
+        keepPage: false,
+        viewportFraction: 0.8);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,83 +53,79 @@ class HomeView extends StatelessWidget {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        height: 230,
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: 6,
-                            physics: BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, position) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: SizedBox(
-                                  width: 230,
-                                  child: Card(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(20.0)),
-                                    child: InkWell(
-                                      borderRadius: BorderRadius.circular(20.0),
-                                      onTap: () {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    ExpandedView()));
-                                      },
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius: BorderRadius.only(
-                                                topLeft: Radius.circular(20.0),
-                                                topRight:
-                                                    Radius.circular(20.0)),
-                                            child: Image.asset(
-                                              "assets/images/webinar.jpg",
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(16.0),
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  "Title",
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .headline6
-                                                      ?.apply(
-                                                          fontWeightDelta: 1),
-                                                ),
-                                                Text(
-                                                  "10th August 2021, 17:00",
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .caption
-                                                      ?.apply(
-                                                          color:
-                                                              Colors.grey[600]),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    height: 220,
+                    child: PageView.builder(
+                        controller: _controller,
+                        itemCount: 6,
+                        onPageChanged: (index) {
+                          setState(() => _currentOngoingPageIndex = index);
+                        },
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, position) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Transform.scale(
+                              scale: position == _currentOngoingPageIndex
+                                  ? 1
+                                  : 0.95,
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0)),
+                                child: InkWell(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ExpandedView()));
+                                  },
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(20.0),
+                                            topRight: Radius.circular(20.0)),
+                                        child: Image.asset(
+                                          "assets/images/webinar.jpg",
+                                        ),
                                       ),
-                                    ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Title",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headline6
+                                                  ?.apply(fontWeightDelta: 1),
+                                            ),
+                                            Text(
+                                              "10th August 2021, 17:00",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .caption
+                                                  ?.apply(
+                                                      color: Colors.grey[600]),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              );
-                            }),
-                      )
-                    ],
+                              ),
+                            ),
+                          );
+                        }),
                   ),
                 ),
               ),
